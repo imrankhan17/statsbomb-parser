beb7593a6c209eb9b9eafb89654246c799e467a1
Khan, Imran, Vodafone Group <imran.khan18@vodafone.com>
2018-09-16 12:31:50 +0100

    ordered output columns

--- statsbomb/parser.py
+++ statsbomb/parser.py
@@ -6,7 +6,7 @@ from statsbomb.utils import columns, get_event_name
 
 class Events:
     """
-
+    Parses json data into tabular format for a particular event.
     """
 
     def __init__(self, file_path: str):
@@ -27,11 +27,14 @@ class Events:
 
         assert event_type.title() in columns['events'], '`{}` is not a valid event type'.format(event_type)
 
+        # get all events for a given event type
         all_events = [i for i in self.data if i['type']['name'] == event_type.title()]
         assert len(all_events) > 0, 'Found 0 events for `{}`'.format(event_type)
 
+        # get common attributes
         common_elements = [{key: event.get(key, None) for key in columns['common']} for event in all_events]
 
+        # get attributes specific to this event type
         event_objects = []
         for event in all_events:
             object_dict = {}
@@ -44,20 +47,23 @@ class Events:
 
         final = [{**i, **j} for i, j in zip(common_elements, event_objects)]
 
+        # combine all into one dataframe and order columns
         df = pd.DataFrame(final)
+        df['event_type'] = event_type
+        df = df[['event_type'] + columns['common'] + columns[event_type]]
 
-        for col in columns['name_cols']:
-            try:
-                df[col] = df[col].apply(get_event_name)
-            except KeyError:
-                pass
+        # get names from attributes of form: `{"id" : 7, "name" : "From Goal Kick"}`
+        name_cols = [i for i in df.columns if i in columns['name_cols']]
+        df[name_cols] = df[name_cols].applymap(get_event_name)
 
+        # split location arrays into their own columns
         try:
             df[['start_location_x', 'start_location_y']] = df['location'].apply(pd.Series)
         except ValueError:
             pass
         df = df.drop('location', axis=1)
 
+        # only the shot event type has a z coordinate
         if 'end_location' in df.columns:
             try:
                 df[['end_location_x', 'end_location_y', 'end_location_z']] = df['end_location'].apply(pd.Series)
--- statsbomb/utils.py
+++ statsbomb/utils.py
@@ -5,7 +5,7 @@ import yaml
 columns = yaml.load(open(os.path.join(os.path.dirname(__file__), 'events.yaml')))
 
 
-def get_event_name(x):
+def get_event_name(x: dict):
     try:
         return x.get('name', None)
     except AttributeError:
